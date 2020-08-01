e9a8a561dbd0d4e63ebfc70ea2c93b53b2de62c1
Aitkeeva Aizhan <aitkeeva.aizhan@gmail.com>
2020-08-01 12:44:41 +0600

    Migrations, fixture, models.py

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = [
+    ('active','Активно'),
+    ('blocked','Заблокировано')
+]
+
+class Record(models.Model):
+    author = models.CharField(max_length=40, null=False, blank=False, verbose_name='Автор')
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='active', verbose_name='Активно')
+    text = models.TextField(max_length=255,null=False, blank=False, verbose_name='Текст')
+    email = models.EmailField(max_length=255, null=False, blank=False, verbose_name='Email')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
+    def __str__(self):
+        return "{}. {}".format(self.pk, self.author, self.email)
+
+    class Meta:
+        verbose_name = "Запись"
+        verbose_name_plural = "Записи"
+
